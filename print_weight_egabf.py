81696a0488b9f589e0abf84de43cc6c6d3a96a5c
HanatoK <summersnow9403@gmail.com>
2022-06-03 14:28:37 -0500

    Use csv in print_weight_egabf.py

--- print_weight_egabf.py
+++ print_weight_egabf.py
@@ -3,11 +3,14 @@
 from histogram import HistogramScalar
 from boltzmann_constant import boltzmann_constant_kcalmolk
 import argparse
+from read_colvars_traj import ReadColvarsTraj
+import numpy as np
+import csv
 
 
 class GetTrajWeightEGABF:
 
-    def __init__(self, position_cols, pmf_filenames, kbt=300.0*boltzmann_constant_kcalmolk):
+    def __init__(self, column_names, pmf_filenames, kbt=300.0*boltzmann_constant_kcalmolk):
         import logging
         import copy
         self.logger = logging.getLogger(self.__class__.__name__)
@@ -16,13 +19,15 @@ class GetTrajWeightEGABF:
         logging_handler.setFormatter(logging_formatter)
         self.logger.addHandler(logging_handler)
         self.logger.setLevel(logging.INFO)
-        self.positionColumns = copy.deepcopy(position_cols)
-        self.maxColumn = max(self.positionColumns)
+        self.column_names = copy.deepcopy(column_names)
+        self.weight_sum = 0.0
+        self.count = 0.0
+        # self.maxColumn = max(self.positionColumns)
         self.pmfs = list()
         self.kbt = kbt
         # self.logger.warning('The weight will not be normalized!')
         # for egABF simulations, all PMFs must be 1D, and the number of PMFs should match the number of position columns
-        if len(position_cols) != len(pmf_filenames):
+        if len(column_names) != len(pmf_filenames):
             raise RuntimeError('The number of PMFs mismatches the number of columns')
         for pmf_filename in pmf_filenames:
             with open(pmf_filename, 'r') as f_pmf:
@@ -33,72 +38,66 @@ class GetTrajWeightEGABF:
                     raise RuntimeError(f'PMF should be 1D in egABF reweighting (file: {pmf_filename})')
                 self.pmfs.append(copy.deepcopy(pmf))
 
-    def parse_traj(self, f_traj, f_output):
-        import numpy as np
+    def accumulate_weights_sum(self, f_traj):
         total_lines = 0
         valid_lines = 0
-        weight_sum = 0
-        count = 0
-        # for the sake of saving memory, I use two round
-        # sum the weights for the first round
-        saved_pos = f_traj.tell()
         for line in f_traj:
-            if line.strip().startswith('#'):
-                continue
             total_lines = total_lines + 1
-            tmp_fields = line.split()
-            if len(tmp_fields) > self.maxColumn:
-                sum_delta_G = 0
-                position_in_grid = True
-                tmp_positions = list()
-                for pmf, col_index in zip(self.pmfs, self.positionColumns):
-                    pos = float(tmp_fields[col_index])
+            position_in_grid = True
+            sum_delta_G = 0
+            tmp_positions = list()
+            for pmf, column_name in zip(self.pmfs, self.column_names):
+                if position_in_grid:
+                    pos = float(line[column_name])
                     tmp_positions.append(pos)
                     tmp_position = [pos]
                     if pmf.is_in_grid(tmp_position):
-                        valid_lines = valid_lines + 1
+                        valid_lines += 1
                         sum_delta_G += pmf[tmp_position]
                     else:
                         sum_delta_G = 0
                         position_in_grid = False
-                        continue
-                weight = np.exp(-1.0 * sum_delta_G / self.kbt)
-                if position_in_grid:
-                    weight_sum += weight
-                    count += 1.0
-                else:
-                    self.logger.warning(f'position {tmp_positions} is not in the boundary.')
+            weight = np.exp(-1.0 * sum_delta_G / self.kbt)
+            if position_in_grid:
+                self.weight_sum += weight
+                self.count += 1.0
             else:
-                raise RuntimeError(f'Maximum column ({self.maxColumn}) is out of bound ({len(tmp_fields)})!')
-        factor = count * 1.0 / weight_sum
-        f_traj.seek(saved_pos)
+                self.logger.warning(f'position {tmp_positions} is not in the boundary.')
+
+    def parse_traj(self, f_traj, f_output, first_time=False, csv_writer=None):
+        factor = self.count * 1.0 / self.weight_sum
+        total_lines = 0
+        valid_lines = 0
         for line in f_traj:
-            if line.strip().startswith('#'):
-                continue
-            tmp_fields = line.split()
-            if len(tmp_fields) > self.maxColumn:
-                sum_delta_G = 0
-                position_in_grid = True
-                tmp_positions = list()
-                for pmf, col_index in zip(self.pmfs, self.positionColumns):
-                    pos = float(tmp_fields[col_index])
+            line['weight'] = 0
+            if csv_writer is None:
+                csv_writer = csv.DictWriter(f_output, fieldnames=line.keys())
+            if first_time:
+                csv_writer.writeheader()
+                first_time = False
+            sum_delta_G = 0
+            position_in_grid = True
+            tmp_positions = list()
+            for pmf, column_name in zip(self.pmfs, self.column_names):
+                if position_in_grid:
+                    pos = float(line[column_name])
                     tmp_positions.append(pos)
                     tmp_position = [pos]
                     if pmf.is_in_grid(tmp_position):
+                        valid_lines += 1
                         sum_delta_G += pmf[tmp_position]
                     else:
                         sum_delta_G = 0
                         position_in_grid = False
                         continue
-                weight = np.exp(-1.0 * sum_delta_G / self.kbt)
-                if position_in_grid:
-                    f_output.write(' '.join(tmp_fields) + f' {factor * weight:22.15e}\n')
-                else:
-                    self.logger.warning(f'position {tmp_positions} is not in the boundary.')
+            line['weight'] = factor * np.exp(-1.0 * sum_delta_G / self.kbt)
+            if position_in_grid:
+                csv_writer.writerow(line)
             else:
-                raise RuntimeError(f'Maximum column ({self.maxColumn}) is out of bound ({len(tmp_fields)})!')
+                self.logger.warning(f'position {tmp_positions} is not in the boundary.')
         self.logger.info(f'Total data lines: {total_lines}')
         self.logger.info(f'Valid data lines: {valid_lines}')
+        return first_time, csv_writer
 
 
 if __name__ == '__main__':
@@ -106,13 +105,18 @@ if __name__ == '__main__':
     required_args = parser.add_argument_group('required named arguments')
     required_args.add_argument('--pmfs', nargs='+', help='egABF 1D PMF file(s)', required=True)
     required_args.add_argument('--traj', nargs='+', help='Colvars trajectory files', required=True)
-    required_args.add_argument('--columns', type=int, nargs='+', help='the columns in the trajectory matching the CVs '
+    required_args.add_argument('--columns', type=str, nargs='+', help='the columns in the trajectory matching the CVs '
                                                                       'of the PMF', required=True)
     required_args.add_argument('--output', help='the output file with weights', required=True)
     parser.add_argument('--kbt', default=300.0*boltzmann_constant_kcalmolk, type=float, help='KbT')
     args = parser.parse_args()
     get_weight_traj = GetTrajWeightEGABF(args.columns, args.pmfs, args.kbt)
+    first_time = True
+    csv_writer = None
+    for traj_file in args.traj:
+        with ReadColvarsTraj(traj_file) as f_traj:
+            get_weight_traj.accumulate_weights_sum(f_traj)
     with open(args.output, 'w') as f_output:
         for traj_file in args.traj:
-            with open(traj_file, 'r') as f_traj:
-                get_weight_traj.parse_traj(f_traj, f_output)
+            with ReadColvarsTraj(traj_file) as f_traj:
+                first_time, csv_writer = get_weight_traj.parse_traj(f_traj, f_output, first_time, csv_writer)
